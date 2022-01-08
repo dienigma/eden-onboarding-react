e1d56d741c3326651beb7d35542cccfe9919fe93
dienigma <joshi.chinmay09@gmail.com>
2022-01-08 17:54:36 +0530

    Plug it in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Heading from './Components/Heading';
 import Container from './Components/Container';
+import OnboardingForm from './Components/OnboardingForm';
 
 export default function App() {
   return (
     <Container>
       <Heading />
+      <p>Bread Crumb Here</p>
+      <OnboardingForm />
     </Container>
   );
 }
