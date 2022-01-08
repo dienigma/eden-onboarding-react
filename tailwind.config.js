b5a8c9cb4c4fac75530dba97aefefd8489240715
dienigma <joshi.chinmay09@gmail.com>
2022-01-08 17:09:49 +0530

    setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'light-gray': '#bdc5d6',
+      'normal-gray': '#9ba0ab',
+      'dark-gray': '#40434a',
+      accent: '#5a4ad1',
+    },
     extend: {},
   },
   plugins: [],
