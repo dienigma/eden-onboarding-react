39758dfcff5853c37e561fc44591a5afb268ea4c
dienigma <joshi.chinmay09@gmail.com>
2022-01-08 17:54:05 +0530

    Extended the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      'light-gray': '#bdc5d6',
-      'normal-gray': '#9ba0ab',
-      'dark-gray': '#40434a',
-      accent: '#5a4ad1',
-      'accent-two': '#2415c9',
-      'accent-lighter': '#b3aee3',
-      'accent-light': '#847cc6',
+    extend: {
+      colors: {
+        'light-gray': '#bdc5d6',
+        'normal-gray': '#9ba0ab',
+        'dark-gray': '#40434a',
+        accent: '#5a4ad1',
+        'accent-two': '#2415c9',
+        'accent-lighter': '#b3aee3',
+        'accent-light': '#847cc6',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
